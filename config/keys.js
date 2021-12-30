3dcc9d9c19657a37994efc27d71f8229e0508a9d
gorkemyalcinkaya <56310746+gorkemyalcinkaya@users.noreply.github.com>
2021-12-30 18:46:19 +0300

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "958573063212-f477ffqofcpfvlb9ksc2vgt7llug7dbl.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI:
-    "mongodb+srv://Admin:kjXBRdVrBHvKlUsq@cluster0.walle.mongodb.net/catSurveyDB?retryWrites=true&w=majority",
-  cookieKey: "qwewqewasdasdzxcqwe",
-};
