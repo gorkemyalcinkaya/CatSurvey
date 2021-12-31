230a3f1e331cecd8f840b41e8ec13348651cb37b
Gorkem Yalcinkaya <gorkemyalcinkaya7@mail.com>
2021-12-31 12:23:25 +0300

    fixing issues

--- index.js
+++ index.js
@@ -70,4 +70,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
